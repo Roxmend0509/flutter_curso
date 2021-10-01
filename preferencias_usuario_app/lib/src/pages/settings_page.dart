0fa91adc5ef305d18b34c0c5c1e1bc70bb45559a
Rox <31557460+Roxmend0509@users.noreply.github.com>
2021-09-30 23:16:49 -0500

    shared preference

--- preferencias_usuario_app/lib/src/pages/settings_page.dart
+++ preferencias_usuario_app/lib/src/pages/settings_page.dart
@@ -1,8 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:preferencias_usuario_app/src/widgets/menu_widget.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   static final String routeName = 'settings';
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  bool _colorSecundario = true;
+  int _genero = 1;
+  String _nombre = 'Pedro';
+  TextEditingController? _textController;
+
+  @override
+  void initState() {
+    super.initState();
+    _cargarPref();
+    _textController = new TextEditingController(text: _nombre);
+  }
+
+  _cargarPref() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    _genero = prefs.getInt('genero') ?? 1;
+    setState(() {});
+  }
+
+  _setSelectedRadio(int? valor) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('genero', valor ?? 1);
+    _genero = valor ?? 1;
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +42,45 @@ class SettingsPage extends StatelessWidget {
           title: Text('Ajustes'),
         ),
         drawer: MenuWidget(),
-        body: Center(
-          child: Text('Ajustes Page'),
+        body: ListView(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(5.0),
+              child: Text('Settings',
+                  style:
+                      TextStyle(fontSize: 45.0, fontWeight: FontWeight.bold)),
+            ),
+            Divider(),
+            SwitchListTile(
+              value: _colorSecundario,
+              title: Text('Color secundario'),
+              onChanged: (value) {
+                _colorSecundario = value;
+                setState(() {});
+              },
+            ),
+            RadioListTile(
+                value: 1,
+                title: Text('Masculino'),
+                groupValue: _genero,
+                onChanged: _setSelectedRadio),
+            RadioListTile(
+                value: 2,
+                title: Text('Femenino'),
+                groupValue: _genero,
+                onChanged: _setSelectedRadio),
+            Divider(),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: TextField(
+                controller: _textController,
+                decoration: InputDecoration(
+                    labelText: 'Nombre',
+                    helperText: 'Nombre de la persona usando el teléfono'),
+                onChanged: (value) {},
+              ),
+            ),
+          ],
         ));
   }
 }
