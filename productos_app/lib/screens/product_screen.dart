9cbad53c96699ffe079d37ac644202e5bf0f9cea
Rox <31557460+Roxmend0509@users.noreply.github.com>
2021-10-06 13:16:25 -0500

    diseño home_page and product_screens

--- productos_app/lib/screens/product_screen.dart
+++ productos_app/lib/screens/product_screen.dart
@@ -1,17 +1,110 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/ui/input_decorations.dart';
+import 'package:productos_app/widgets/product_image.dart';
 
 class ProductScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: Column(
-        children: [
-          Stack(
-            children: [],
-          )
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                ProductImage(),
+                Positioned(
+                  child: IconButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      icon: Icon(
+                        Icons.arrow_back_ios_new,
+                        size: 40,
+                        color: Colors.white,
+                      )),
+                  top: 60,
+                  left: 20,
+                ),
+                Positioned(
+                  child: IconButton(
+                      onPressed: () {
+                        // TODO: Camara o galería
+                      },
+                      icon: Icon(
+                        Icons.camera_alt_outlined,
+                        size: 40,
+                        color: Colors.white,
+                      )),
+                  top: 60,
+                  right: 30,
+                )
+              ],
+            ),
+            _ProductForm(),
+            SizedBox(
+              height: 100,
+            )
+          ],
+        ),
       ),
-    ));
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.save_outlined),
+        onPressed: () {
+          //TODO: Guardar
+        },
+      ),
+    );
   }
+}
+
+class _ProductForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        width: double.infinity,
+        decoration: _buildBoxDecoration(),
+        child: Form(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                  decoration: InputDecorations.authInputDecoration(
+                      hintText: 'Nombre del producto', labelText: 'Nombre:')),
+              TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecorations.authInputDecoration(
+                      hintText: '\$150', labelText: 'Precio:')),
+              SizedBox(
+                height: 30,
+              ),
+              SwitchListTile(
+                  value: true,
+                  title: Text('Disponible'),
+                  onChanged: (value) {
+                    //TODO: Pendiente
+                  }),
+              SizedBox(
+                height: 30,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration _buildBoxDecoration() => BoxDecoration(
+      color: Colors.white,
+      boxShadow: [
+        BoxShadow(
+            color: Colors.black.withOpacity(0.05),
+            offset: Offset(0, 5),
+            blurRadius: 5)
+      ],
+      borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(25), bottomRight: Radius.circular(25)));
 }
