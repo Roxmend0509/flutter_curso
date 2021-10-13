cf191b7bbd0ebafe50b667dfb77148b756fb154d
Rox <31557460+Roxmend0509@users.noreply.github.com>
2021-10-13 17:45:19 -0500

    initial Values

--- productos_app/lib/screens/product_screen.dart
+++ productos_app/lib/screens/product_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/providers/providers.dart';
 import 'package:productos_app/services/services.dart';
 import 'package:productos_app/ui/input_decorations.dart';
 import 'package:productos_app/widgets/product_image.dart';
@@ -9,6 +10,27 @@ class ProductScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final productService = Provider.of<ProductsService>(context);
 
+    return ChangeNotifierProvider(
+      create: (_) => ProductFormProvider(productService.selectedProduct),
+      child: _ProductScreenBody(
+        productService: productService,
+      ),
+    );
+
+    //return _ProductScreenBody(productService: productService);
+  }
+}
+
+class _ProductScreenBody extends StatelessWidget {
+  const _ProductScreenBody({
+    Key? key,
+    required this.productService,
+  }) : super(key: key);
+
+  final ProductsService productService;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
@@ -63,6 +85,8 @@ class ProductScreen extends StatelessWidget {
 class _ProductForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final productForm = Provider.of<ProductFormProvider>(context);
+    final product = productForm.product;
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 10),
       child: Container(
@@ -76,17 +100,31 @@ class _ProductForm extends StatelessWidget {
                 height: 10,
               ),
               TextFormField(
+                  initialValue: product.name,
+                  onChanged: (value) => product.name = value,
+                  validator: (value) {
+                    if (value == null || value.length < 1)
+                      return 'El nombre es obligatorio';
+                  },
                   decoration: InputDecorations.authInputDecoration(
                       hintText: 'Nombre del producto', labelText: 'Nombre:')),
               TextFormField(
+                  initialValue: '${product.price}',
+                  onChanged: (value) {
+                    if (double.tryParse(value) == null) {
+                      product.price = 0;
+                    } else {
+                      product.price = double.parse(value);
+                    }
+                  },
                   keyboardType: TextInputType.number,
                   decoration: InputDecorations.authInputDecoration(
                       hintText: '\$150', labelText: 'Precio:')),
               SizedBox(
                 height: 30,
               ),
-              SwitchListTile(
-                  value: true,
+              SwitchListTile.adaptive(
+                  value: product.available,
                   title: Text('Disponible'),
                   onChanged: (value) {
                     //TODO: Pendiente
